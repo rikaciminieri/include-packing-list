fa569f913321bbeb52e9984441ac7d614acadad6
Rika Ciminieri <rikaciminieri@gmail.com>
2022-06-15 19:53:15 -0700

    Add submit handler

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [userInput, setUserInput] = useState({
+  const initialInputState = {
     item: "",
     quantity: 0,
-  });
+  };
+  const [userInput, setUserInput] = useState(initialInputState);
 
   const [packingList, setPackingList] = useState([]);
 
@@ -18,6 +19,15 @@ export default function Home() {
 
     console.log(userInput);
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setPackingList([userInput, ...packingList]);
+
+    setUserInput(initialInputState);
+  };
+
   return (
     <div>
       <h3>Travel Must-Haves</h3>
@@ -27,19 +37,28 @@ export default function Home() {
           placeholder="Type an item"
           onChange={handleChange}
           name="item"
+          value={userInput.item}
         />
         <input
           type="number"
           placeholder="Quantity"
           onChange={handleChange}
           name="quantity"
+          value={userInput.quantity}
         />
-        <button type="submit">Add</button>
+        <button type="submit" onClick={handleSubmit}>
+          Add
+        </button>
       </form>
       <ul>
         {packingList.length >= 1
           ? packingList.map((todo, idx) => {
-              return <li key={idx}>{todo}</li>;
+              return (
+                <li key={idx}>
+                  <input type="checkbox" />
+                  {todo.quantity} {todo.item}{" "}
+                </li>
+              );
             })
           : "Add an item and start packing!"}
       </ul>
