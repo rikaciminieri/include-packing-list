2c3d0bcdfd85136596fa23207e583beea62b78b1
Rika Ciminieri <rikaciminieri@gmail.com>
2022-06-15 19:24:53 -0700

    Map out HTML

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [packingList, setPackingList] = useState([]);
+
   return (
     <div>
-      Hiya
+      <h3>Travel Must-Haves</h3>
+      <form>
+        <input type="text" placeholder="Type an item" />
+        <input type="number" placeholder="Quantity" />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {packingList.length >= 1
+          ? packingList.map((todo, idx) => {
+              return <li key={idx}>{todo}</li>;
+            })
+          : "Add an item and start packing!"}
+      </ul>
     </div>
-  )
+  );
 }
