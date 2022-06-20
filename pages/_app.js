21b3b06620b39c2cf544d4980e3aa909c558e3b5
Rika Ciminieri <rikaciminieri@gmail.com>
2022-06-19 18:45:37 -0700

    Application spans height of viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/tailwind.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="h-screen">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
