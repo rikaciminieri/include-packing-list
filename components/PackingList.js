9c04b91898bee7ec4e08666ab45294f558cded13
Rika Ciminieri <rikaciminieri@gmail.com>
2022-06-19 18:25:17 -0700

    Move relevant packing list logic to PackingList.js

--- components/PackingList.js
+++ components/PackingList.js
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 const PackingList = ({ packingList, setPackingList }) => {
+  // Value will be id being edited
+  const [itemEditingID, setItemEditingID] = useState(null);
+  const handleDelete = (id) => {
+    const updatedPackingList = packingList.filter((listItem) => {
+      return listItem.id !== id;
+    });
+
+    setPackingList(updatedPackingList);
+  };
+
+  return (
+    <div>
+        <p>Add an item and start packing!</p>
+    </div>
+  );
 };
 
 export default PackingList;
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ import { v4 as uuidv4 } from "uuid";
 export default function Home() {
 
   const [packingList, setPackingList] = useState([]);
-  // Value will be id we are editing
-  const [itemEditing, setItemEditing] = useState(null);
   const [editingText, setEditingText] = useState({});
 
   const handleToggle = (id) => {
@@ -19,14 +17,6 @@ export default function Home() {
         : listItem;
     });
     setPackingList(mapped);
-  };
-
-  const handleDelete = (id) => {
-    const updatedPackingList = packingList.filter((listItem) => {
-      return listItem.id !== id;
-    });
-
-    setPackingList(updatedPackingList);
   };
 
   const handleEditingChange = (event) => {
@@ -50,84 +40,6 @@ export default function Home() {
   return (
     <div>
       <h3>Travel Must-Haves</h3>
-      <ul>
-        {packingList.length >= 1
-          ? packingList
-              .filter(({ isPacked }) => !isPacked)
-              .map(({ item, id, quantity, isPacked }) => {
-                return (
-                  <div key={id} className={cx("item", { isPacked })}>
-                    <input
-                      type="checkbox"
-                      defaultChecked={isPacked}
-                      onClick={() => handleToggle(id)}
-                    />
-                    {id === itemEditing ? (
-                      <div>
-                        <input
-                          type="text"
-                          onChange={handleEditingChange}
-                          value={editingText.item}
-                          name="item"
-                        />
-                        <input
-                          type="number"
-                          onChange={handleEditingChange}
-                          value={editingText.quantity}
-                          name="quantity"
-                        />
-                      </div>
-                    ) : (
-                      <li>
-                        {quantity} {item}
-                      </li>
-                    )}
-
-                    {itemEditing === id ? (
-                      <button onClick={() => editListItem(id)}>Done</button>
-                    ) : (
-                      <button onClick={() => setItemEditing(id)}>Edit</button>
-                    )}
-
-                    <button
-                      onClick={(e) => {
-                        e.preventDefault();
-                        handleDelete(id);
-                      }}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                );
-              })
-          : "Add an item and start packing!"}
-
-        {packingList
-          .filter(({ isPacked }) => isPacked)
-          .map(({ item, id, quantity, isPacked }) => {
-            return (
-              <div key={id} className={cx("item", { isPacked })}>
-                <input
-                  type="checkbox"
-                  defaultChecked={isPacked}
-                  onClick={() => handleToggle(id)}
-                />
-                <li>
-                  {quantity} {item}
-                </li>
-                <button>Edit</button>
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleDelete(id);
-                  }}
-                >
-                  Delete
-                </button>
-              </div>
-            );
-          })}
-      </ul>
     </div>
   );
 }
